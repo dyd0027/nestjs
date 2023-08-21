4780ab8364ddccf74be93f04a400cc012de30bf5
dyd0027 <dyd002700@gmail.com>
2023-08-21 17:28:32 +0900

    2.3Header컴포넌트, typescript props

---
+++ section00/pages/feedback.tsx
@@ -0,0 +1,11 @@
+import { Fragment } from 'react';
+import Header from '../components/common/Header';
+import styles from '../styles/header.module.scss';
+export default function FeedBack() {
+  return (
+    <Fragment>
+      <Header/>
+      <main></main>
+    </Fragment>
+  );
+}
--- section00/pages/index.tsx
+++ section00/pages/index.tsx
@@ -1,10 +1,17 @@
 import { Fragment } from 'react';
 import Header from '../components/common/Header';
+import styles from '../styles/header.module.scss';
+import Link from 'next/link';
+import {AiOutlineCopy} from 'react-icons/ai';
+import {MdOutlineFeedback} from 'react-icons/md';
 
 export default function Home() {
   return (
     <Fragment>
-      <Header />
+      <Header rightElements={[
+        <button onClick={()=>alert('복사누름')} className={styles.box}><AiOutlineCopy size={20}/></button>,
+        <Link href='/feedback'><MdOutlineFeedback/></Link>,
+      ]} />
       <main></main>
     </Fragment>
   );
